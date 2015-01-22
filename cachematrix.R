9feb2cf8e30800efe9865f9200efefdc8306421f
diegocgaona <diego@diegogaona.com>
2015-01-21 22:51:23 -0200

    Cachematrix.R Function to calc inverse matrix and use cache
    This functions use a cache to optimize the solve of a inverse matrix
    without computate all again.

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,49 @@
-## Put comments here that give an overall description of what your
-## functions do
+## This functions use a cache to optimize the solve
+## of a inverse matrix without computate all again.
 
-## Write a short comment describing this function
+## This function creates a special "matrix" 
+## object that can cache its inverse.
 
 makeCacheMatrix <- function(x = matrix()) {
-
+      
+      i <- NULL # where we gone to store the inverse matrix
+      
+      set <- function(y) { ## Set the vector x to the value passed in.
+            x <<- y ## <<- to assign x in higher environment
+            i <<- NULL ## to reset m to NULL
+      }
+      
+      get <- function() x   ## return the value of x stored in the
+      ## makeVector object
+      
+      setimatrix <- function(imatrix) i <<- imatrix ## set the value of m
+      getimatrix <- function() i
+      
+      list(set = set,       ## creates list of nested functions with tags
+           get = get,       
+           setimatrix = setimatrix,
+           getimatrix = getimatrix)
 }
 
-
-## Write a short comment describing this function
+## This function will read a cached inverse matrix if it already
+## exists, or calculate if it don't.
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+      i <- x$getimatrix() ## Use getimatrix() function in makeCacheMatrix
+      
+      if(!is.null(i)) {  ## If i is not NULL, gets cached inverse matrix
+            
+            message("getting cached data")
+            return(i)      ## return cached inverse matrix
+      }
+      
+      data <- x$get()
+      i <- solve(data, ...)  ## calculate the mean of data passing in any 
+      ## additional parameters received.
+      
+      x$setimatrix(i)   ## cache the newly caclulated inverse matrix 
+                        ## in makeVector
+      
+      i                 ## return newly calculated inverse matrix
 }
